5c83e5a536d0e340f6fed20f91e06d9e6a625a72
IkramahAhmed <ikramahahmed177@gmail.com>
2023-06-13 01:36:11 +0500

    complete

---
+++ js/main.js
@@ -0,0 +1,44 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// Owl Carousel
+$(document).ready(function() {
+    $(".client-slider-section").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 6
+            }
+        }
+    });
+});
+
+//seen yeh hota he jab boootstrapp apnnay start men jaisai kise pr click hota he to wh show ke class append krleta he to ise waja say navbar kise pr click krnay say bund nai hota to bund krnay kay liay hum usclass ko remove karwa dengay show wale
+
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
